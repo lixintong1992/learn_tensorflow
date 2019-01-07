8088349c739245b17809320c5f9194660460eda3
lixintong <18392181430@163.com>
2019-01-08 00:10:47 +0800

    add mnist with summaries

--- mnist_tf.py
+++ mnist_tf.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
     
+    ipdb.set_trace()
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
